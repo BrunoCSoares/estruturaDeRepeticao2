436d67ead12f700ecd0b1375a8793922e9026ede
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-09-19 21:14:08 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -3,3 +3,12 @@ Escreva um programa que calcule o somatório de todos os números pares em
 um intervalo definido pelo usuário. Ex: para inicio = 2 e fim = 10 o somatório é
 2+4+6+8+10. OBS: utilize o “for”.
 '''
+
+numI = int(input("Informe o número inicial: "))
+numF = int(input("Informe o número final: "))
+soma = 0
+
+for i in range(numI,numF+1):
+    if i % 2 == 0:
+        soma += i
+    print(soma)
