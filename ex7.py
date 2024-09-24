aa3461f1d74b255448d96fae88cbc616883470a0
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-09-23 21:36:03 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -5,11 +5,14 @@ usuário.
 Conforme exemplo abaixo:
 Digite um número inteiro positivo: 5
 5 x 0=0
-Disciplina: Computacional Thinking using Python
-Prof. Dr. Daniel Trevisan Bravo
 5 x1 =5
 5 x2=10
 5 x3=15
 …
 5 x10=50
 '''
+
+n = int(input("Insira um número inteiro: "))
+
+for i in range(1,10+1):
+    print(f"{n} x {i} = {n*i}")
