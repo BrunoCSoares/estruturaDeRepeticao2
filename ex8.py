cb7858d1173265b7ca9f1b94facc5d30cd971563
Bruno Carlos Soares <122658439+BrunoCSoares@users.noreply.github.com>
2024-09-23 22:22:12 -0300

    Update ex8.py

--- ex8.py
+++ ex8.py
@@ -11,3 +11,49 @@ cidades pertencem
 b) qual a média de veículos nas cidades da região Sudeste
 c) qual a média de acidentes com vítimas na cidade de São Paulo
 '''
+
+nomeCidade = [None] * 4
+numCarros = [None] * 4
+numVitimas = [None] * 4
+
+for i in range(4):
+    cidade = input("Insita o nome da cidade: ")
+    carros = int(input("Insira o número de carros de passeio nesta cidade: "))
+    vitimas = int(input("Insira o número de acidentes de trânsito com vítimas: "))
+    nomeCidade[i] = cidade
+    numCarros[i] = carros
+    numVitimas[i] = vitimas
+    print("\n")
+
+maiorIndice = numVitimas[0]
+for i in range(3):
+    if numVitimas[i] > numVitimas[i+1] and numVitimas[i] > maiorIndice:
+        maiorIndice = numVitimas[i]
+    elif numVitimas[i+1] > maiorIndice:
+        maiorIndice = numVitimas[i+1]
+
+menorIndice = numVitimas[0]
+for i in range(3):
+    if numVitimas[i] < numVitimas[i+1] and numVitimas[i] < menorIndice:
+        menorIndice = numVitimas[i]
+    elif numVitimas[i+1] < menorIndice:
+        menorIndice = numVitimas[i+1]
+
+print(f"O maior indice de acidentes é {maiorIndice}")
+print(f"O menor indice de acidentes é {menorIndice}")
+
+mediaCarros = 0
+for i in range(4):
+    mediaCarros += numCarros[i]/4
+
+print(f"A média de carros na região sudeste é {mediaCarros}")
+
+SpVitimas = "sp"
+for i in range(4):
+    if nomeCidade[i][0] == "s":
+        SpVitimas = numVitimas[i]
+
+if SpVitimas == "sp":
+    print("SP não encontrado")
+else:
+    print(f"O número de vítimas em SP é {SpVitimas}")
